851db9bcf3fbe414d3f7a19559b1ba62f52f1b50
mkdash2006 <mkdash2006@gmail.com>
2026-09-05 13:07:03 +0530

    new change

--- conftest.py
+++ conftest.py
@@ -2,15 +2,15 @@
 from playwright.sync_api import Page, sync_playwright
 import pytest
 
-@pytest.fixture()
-def navigate_to_amazon(page: Page):
-    page.goto("https://www.amazon.in/")
-    page.wait_for_timeout(3000)
-# # If any interim page will come before opening homepage. 
-# # eX- timeout page, continue page
-#     countofbtns = page.locator('//*[contains(text(),"shopping")]').count()  
-#     if countofbtns > 0:
-#         page.locator('//*[contains(text(),"Shopping")]').click()
+# @pytest.fixture()
+# def navigate_to_amazon(page: Page):
+#     page.goto("https://www.amazon.in/")
+#     page.wait_for_timeout(3000)
+# # # If any interim page will come before opening homepage. 
+# # # eX- timeout page, continue page
+# #     countofbtns = page.locator('//*[contains(text(),"shopping")]').count()  
+# #     if countofbtns > 0:
+# #         page.locator('//*[contains(text(),"Shopping")]').click()
 
 
 # @pytest.fixture(autouse=True)     
